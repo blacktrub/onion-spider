08e26f0dce6f267389ee1f9e4b37af80e332f7d9
blacktrub <lightlocdog@gmail.com>
2021-06-27 16:23:42 +0300

    keep sites in db

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+import sqlite3
 from urllib.parse import urlparse
 
 import requests
@@ -10,6 +11,17 @@ PROXIES = {
     "https": "socks5h://127.0.0.1:9050",
 }
 GLOBAL_UNIQ = set()
+
+
+def create_schema(connect):
+    cur = connect.cursor()
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS sites 
+        (id integer primary key autoincrement, parent_id integer, domain text unique)
+    """
+    )
+    connect.commit()
 
 
 def get_first_domain(link):
@@ -47,19 +59,39 @@ def request_page(url):
     return requests.get(url, timeout=20, proxies=PROXIES)
 
 
-if __name__ == "__main__":
+def add_site(cur, domain, parent_id=None):
+    cur.execute(
+        """
+        INSERT INTO sites(parent_id, domain) VALUES (?, ?)
+    """,
+        (parent_id, domain),
+    )
+
+
+def get_site(cur, domain):
+    cur.execute("select id from sites where domain=?", (domain,))
+    return cur.fetchone()[0]
+
+
+def parser(connect):
     url = "http://dirnxxdraygbifgc.onion/"
     queue = set()
+    cur = connect.cursor()
     for link in fetch_links(request_page(url)):
         queue.add(link)
+        parse_result = urlparse(link)
+        add_site(cur, parse_result.netloc)
+    connect.commit()
 
     while queue:
         print("queue size", len(queue))
         link = queue.pop()
+        db_id = get_site(cur, urlparse(link).netloc)
         print("work with", link)
         try:
             page = request_page(link)
         except (SSLError, ConnectionError, ConnectTimeout):
+            print("skip with error")
             continue
 
         for link in fetch_links(page):
@@ -68,3 +100,17 @@ if __name__ == "__main__":
 
             GLOBAL_UNIQ.add(link)
             queue.add(link)
+            add_site(cur, urlparse(link).netloc, db_id)
+        connect.commit()
+
+        for x in cur.execute("select * from sites"):
+            print(x)
+
+
+if __name__ == "__main__":
+    con = sqlite3.connect("parser.db")
+    try:
+        create_schema(con)
+        parser(con)
+    finally:
+        con.close()
